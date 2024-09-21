9b70fec7b298c1bc37282f8e3de8901caa1919cd
Yanissou <yanissoumc@gmail.com>
2024-09-21 15:49:35 +0200

    try to break default value

--- src/main/java/fr/yanissou/lespainsapi/LespainsapiApplication.java
+++ src/main/java/fr/yanissou/lespainsapi/LespainsapiApplication.java
@@ -12,7 +12,7 @@ public class LespainsapiApplication {
         SpringApplication.run(LespainsapiApplication.class, args);
 
         //Create file if it doesn't exist
-        File file = new File(System.getenv().getOrDefault("FILE_PATH", "/app/messages.txt"));
+        File file = new File(System.getenv().getOrDefault("FILE_PATH", "hahahahaa"));
 
         File parentDir = file.getParentFile();
         if (!parentDir.exists()) {
