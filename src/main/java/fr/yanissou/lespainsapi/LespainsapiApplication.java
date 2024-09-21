c87d5dad7e15edd96b597464cd90c7a280c70b48
Yanissou <yanissoumc@gmail.com>
2024-09-21 15:58:18 +0200

    Added HelloWorld page

--- src/main/java/fr/yanissou/lespainsapi/LespainsapiApplication.java
+++ src/main/java/fr/yanissou/lespainsapi/LespainsapiApplication.java
@@ -2,6 +2,8 @@ package fr.yanissou.lespainsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.File;
 
@@ -28,4 +30,8 @@ public class LespainsapiApplication {
         }
     }
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
 }
