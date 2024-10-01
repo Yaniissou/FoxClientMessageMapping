dc9c5b20b2233111bf2e8c36dacc94800cdc82b7
Yanissou <yanissoumc@gmail.com>
2024-10-01 20:39:27 +0200

    removed file creation

--- src/main/java/fr/yanissou/lespainsapi/LespainsapiApplication.java
+++ src/main/java/fr/yanissou/lespainsapi/LespainsapiApplication.java
@@ -12,16 +12,5 @@ public class LespainsapiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LespainsapiApplication.class, args);
-
-        //Create file if it doesn't exist
-        File file = new File(System.getenv().getOrDefault("FILE_PATH", "/app/messages.txt"));
-
-        if (!file.exists()) {
-            try {
-                file.createNewFile();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
     }
 }
